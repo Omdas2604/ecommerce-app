270344840930cc45efea1b2ad50cb7ead8c6769a
Omdas2604 <dasswayenshuom@gmail.com>
2025-09-07 14:37:51 +0530

    Backend

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function NavBar({ user, onLogout }){
+export default function NavBar({ user, onLogout, quantity }){
+
+
+
   return (
     <nav style={{ display:'flex', gap:15, padding:12, borderBottom:'1px solid #eee' }}>
       <Link to="/">Products</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart">Cart({quantity || 0})</Link>
       {
         user ? (
           <>
